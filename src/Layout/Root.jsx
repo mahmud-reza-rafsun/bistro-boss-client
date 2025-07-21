cce9780843a28c7c3a2066a4fb68d60af5a1fe6d
mahmud-reza-rafsun <rafsun16.it@gmail.com>
2025-07-21 09:40:16 +0600

    first commit

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
+import Footer from "../Pages/Shared/Footer/Footer";
+
+const Root = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="min-h-[calc(100vh-280px)]">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Root;
